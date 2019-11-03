2e6e30926c01f55fcc00cb28c995fcbf289150ad
germanrojas21 <germanrojas21@gmail.com>
2019-11-02 22:17:13 -0500

    Cambios realizados por verificación de código en la W3C

---
+++ website/Recursos/Javascript/Scripts.js
@@ -0,0 +1,28 @@
+﻿$(function () {
+
+    var txtNombre = document.getElementById("txtNombre");
+    var txtApellidos = document.getElementById("txtApellidos");
+    var txtDireccion = document.getElementById("txtDireccion");
+    var txtEmail = document.getElementById("txtEmail");
+    var txtDireccionDenuncia = document.getElementById("txtDireccionDenuncia");
+    var txtNombreImplicado = document.getElementById("txtNombreImplicado");
+
+    if (txtNombre.value == '')
+        alert("El campo Nombre es requerido.");
+
+    if (txtApellidos.value == '')
+        alert("El campo Apellidos es requerido.");
+
+    if (txtDireccion.value == '')
+        alert("El campo Dirección es requerido.");
+
+    if (txtEmail.value == '')
+        alert("El campo Email es requerido.");
+
+    if (txtDireccionDenuncia.value == '')
+        alert("El campo Dirección del lugar afectado es requerido.");
+
+    if (txtNombreImplicado.value == '')
+        alert("El campo Nombre del implicado es requerido.");
+
+});
